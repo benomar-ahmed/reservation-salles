277980eba7e1983481661ef6323a07c283c6515b
benomar-ahmed <ben-omar.ahmed@laplateforme.io>
2022-12-21 16:29:43 +0100

    Connection in plesk and FInish

--- connect.php
+++ connect.php
@@ -2,9 +2,9 @@
 
 /*#######       BASE DE DONNEES     #######*/
 
-$mysqli = new mysqli("localhost","root","","reservationsalles");
+// $mysqli = new mysqli("localhost","root","","reservationsalles");
 
-
+$mysqli = mysqli_connect("localhost","benomar-ahmed","ahmedAHMED1234","ben-omar-ahmed_reservation-salles", "3306");
 
 
 
