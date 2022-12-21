86ef1c61c5d7044aae71f3b8189df676deeb3a47
benomar-ahmed <ben-omar.ahmed@laplateforme.io>
2022-12-21 16:07:12 +0100

    Update files with file css and add a new file css

--- footer.php
+++ footer.php
@@ -4,13 +4,14 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="footer.css">
     <title>Footer</title>
 </head>
-<body>
+<body id="body-footer">
     <footer>
         <p>Page créer par AHMED Ben-Omar</p>
-        <a href="https://github.com/benomar-ahmed/reservation-salles"><img src="Images/icon-github.png" alt="Logo github"></a>
-        <img src="Images/logo_laplateforme.png" alt="Logo de La Plateforme">
+        <a href="https://github.com/benomar-ahmed/reservation-salles" id="github"><img src="Images/icon-github.png" alt="Logo github" style="width:100% ;"></a>
+        <img src="Images/logo_laplateforme.png" alt="Logo de La Plateforme" id="laplateforme">
     </footer>
 </body>
 </html>
